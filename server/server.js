f2bdf6e78e19bd92a032546691404d4508421ba4
Saubhagya <saubhagya@Saubhagyas-MacBook-Air-M2.local>
2023-01-04 00:28:54 +0530

    update api key direct

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
