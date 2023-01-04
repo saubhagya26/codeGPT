6773a4974d2e39536ce52fe16f8bc76e72742f21
Saubhagya <saubhagya@Saubhagyas-MacBook-Air-M2.local>
2023-01-04 16:54:29 +0530

    Removed .env

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server -> bot's response
-  const response = await fetch('https://codegpt-9aaq.onrender.com/', {
+  const response = await fetch('http://localhost:8000', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
